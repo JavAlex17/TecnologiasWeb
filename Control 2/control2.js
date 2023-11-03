94ae444063d3dcbd77e183ae9ff06126ab3ffaf0
JavAlex17 <72239869+JavAlex17@users.noreply.github.com>
2023-11-03 13:06:03 -0300

    intento 1

---
+++ Control 2/control2.js
@@ -0,0 +1,30 @@
+
+
+let cantidad = document.getElementById("num");
+
+let boton_may = document.getElementById("mayus");
+let boton_min = document.getElementById("minus");
+let boton_esp = document.getElementById("c_esp");
+let boton_gen = document.getElementById("generar");
+
+boton_may.addEventListener("click", () => {
+
+})
+
+boton_min.addEventListener("click", () => {
+
+})
+
+boton_esp.addEventListener("click", () => {
+
+})
+
+
+boton_gen.addEventListener("click", () => {
+    window.alert("Generando Contraseña");
+    generar_Contraseña();
+
+  })
+
+  function generar_Contraseña(){
+  }
